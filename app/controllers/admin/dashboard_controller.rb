a5de16c994fb03bb4f77bc93e37f9b6f186577e3
Carol Cheung <carol.cheung@telus.com>
2023-01-24 21:54:04 -0800

    add admin dashboard with product and category counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products_count = Product.count
+    @category_count = Category.count 
+    puts @products_count
+    puts @category_count
   end
+
 end
