0856baad066195b03ed7b3a98210abebbed3ce1f
Carol Cheung <carol.cheung@telus.com>
2023-01-30 16:00:42 -0800

    add user rspec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
-
-  
   has_secure_password
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :password_digest, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
 
 end
